08399e98a835e021e5442bc0aa238486e48a1d96
weiseng18 <tws.hci@gmail.com>
2022-05-21 23:40:11 +0800

    nvim: add lsp bindings

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -3,6 +3,28 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(
 )
 
 local nvim_lsp = require('lspconfig')
+local set_keymap = function(bufnr, ...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+end
+local opts = { noremap = true, silent = true }
+
+-- lsp-specific keymaps
+local on_attach = function(_, bufnr)
+  set_keymap(bufnr, "n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts)
+  set_keymap(bufnr, "n", "gD", ":lua vim.lsp.buf.declaration()<CR>", opts)
+  set_keymap(bufnr, "n", "gt", ":lua vim.lsp.buf.type_definition()<CR>", opts)
+  set_keymap(bufnr, "n", "gr", ":lua vim.lsp.buf.references()<CR>", opts)
+  set_keymap(bufnr, "n", "gi", ":lua vim.lsp.buf.implementation()<CR>", opts)
+  set_keymap(bufnr, "n", "K", ":lua vim.lsp.buf.hover()<CR>", opts)
+  print("LSP attached!")
+end
+
+vim.diagnostic.config({ float = { source = 'always', border = border }, })
+
+-- javascript lsp
 nvim_lsp.tsserver.setup {
+  on_attach = function(_, bufnr)
+    on_attach(_, bufnr)
+  end,
   capabilities = capabilities,
 }
